1bbc8e9b3ea02d8fe33cc7c294ac65d079053da7
d02d33pak <deepak030190@gmail.com>
2020-03-25 01:03:00 +0530

    problem01

---
+++ 01_problem.py
@@ -0,0 +1,18 @@
+# method 01 takes 0.00018180100050813053 time 
+multiples = []
+for i in range(1, 1000):
+    if i % 3 == 0 or i % 5 == 0:
+        # summ += i
+       multiples.append(i) 
+
+print(sum(multiples))
+
+
+# method 02 takes 0.0003612910004449077 time
+# summ = 0
+# for i in range (1, 1000):
+#     if i % 3 == 0 or i % 5 == 0:
+#         # summ += i
+#        multiples.append(i) 
+
+# print(summ)
