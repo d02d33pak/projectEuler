1317454608d5f04608649fb7ec73f3b6adf7a5a5
d02d33pak <deepak030190@gmail.com>
2020-03-25 21:40:25 +0530

    problem 02 solution

---
+++ problem02/problem02.py
@@ -0,0 +1,20 @@
+# a little easy to understand
+
+# a, b, c, total = 1, 1, 0, 0
+# while (c < 4000000):
+#     c =  a + b
+#     if c % 2 == 0:
+#         total += c
+#     a, b = b , c
+
+
+# same but little more efficient
+
+a, b, total = 1, 1, 0
+while(a < 4000000):
+    if a % 2 == 0:
+        total += a
+    a,b = b,a+b
+
+print(total)
+
